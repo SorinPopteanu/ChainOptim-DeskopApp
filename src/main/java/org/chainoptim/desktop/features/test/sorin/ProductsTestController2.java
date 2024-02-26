61d038fe0a15aa9d897a3613ee5ddf150abec2e5
Sorin Popteanu <sorinpopteanu72@gmail.com>
2024-02-26 11:46:27 +0200

    The method that creates buttons dynamically

--- src/main/java/org/chainoptim/desktop/features/test/sorin/ProductsTestController2.java
+++ src/main/java/org/chainoptim/desktop/features/test/sorin/ProductsTestController2.java
@@ -3,7 +3,12 @@ package org.chainoptim.desktop.features.test.sorin;
 import com.google.inject.Inject;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
 import org.chainoptim.desktop.core.context.TenantContext;
 import org.chainoptim.desktop.features.product.model.Product;
 import org.chainoptim.desktop.features.product.repository.ProductRepository;
@@ -22,13 +27,29 @@ public class ProductsTestController2 implements Initializable {
         this.productRepository = productRepository;
     }
 
+    @FXML
+    private TilePane productsTilePane;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         if (TenantContext.getCurrentUser() != null) {
+
             Integer organizationId = TenantContext.getCurrentUser().getOrganization().getId();
-            System.out.println(organizationId);
             Optional<List<Product>> products = productRepository.getProductsByOrganizationId(organizationId);
-            System.out.println(products);
+
+            if (products.isPresent()) {
+                for (Product product : products.get()) {
+                    Label productName = new Label(product.getName());
+                    productName.getStyleClass().add("name-label");
+                    Label productDescription = new Label(product.getDescription());
+                    productDescription.getStyleClass().add("description-label");
+                    VBox productBox = new VBox(productName, productDescription);
+                    Button productButton = new Button();
+                    productButton.setGraphic(productBox);
+
+                    productsTilePane.getChildren().add(productButton);
+                }
+            }
         }
 
     }
