02298a4fe39df197008604f36a40a6da11dae171
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-03-06 19:28:58 +0200

    Attempt fixing Maven not recognizing tests

--- src/test/java/org/chainoptim/desktop/core/main/service/NavigationServiceTest.java
+++ src/test/java/org/chainoptim/desktop/core/main/service/NavigationServiceTest.java
@@ -1,13 +1,10 @@
 package org.chainoptim.desktop.core.main.service;
 
-import org.chainoptim.desktop.core.abstraction.ControllerFactory;
-import org.chainoptim.desktop.core.abstraction.ThreadRunner;
-import org.chainoptim.desktop.core.main.service.NavigationServiceImpl;
-import org.chainoptim.desktop.shared.util.resourceloader.FXMLLoaderService;
-
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-
+import org.chainoptim.desktop.core.abstraction.ControllerFactory;
+import org.chainoptim.desktop.core.abstraction.ThreadRunner;
+import org.chainoptim.desktop.shared.util.resourceloader.FXMLLoaderService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,9 +17,6 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-/*
- * Not functional
- */
 @ExtendWith(MockitoExtension.class)
 public class NavigationServiceTest {
 
