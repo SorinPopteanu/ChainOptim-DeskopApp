9dcf40373c8cd83fd8f27271241e341471ffe304
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-03-07 22:52:03 +0200

    Add write repositories for Supplier and Warehouse

---
+++ src/main/java/org/chainoptim/desktop/features/warehouse/dto/CreateWarehouseDTO.java
@@ -0,0 +1,11 @@
+package org.chainoptim.desktop.features.warehouse.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateWarehouseDTO {
+
+    private String name;
+    private Integer organizationId;
+}
+
