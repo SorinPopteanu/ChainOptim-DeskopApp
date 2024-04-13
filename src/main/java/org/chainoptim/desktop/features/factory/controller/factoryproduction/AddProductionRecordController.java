ef7643ed051b8d7f2d78ac4e712dc76e31548e9b
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-04-13 11:30:40 +0300

    Fix issue with picking dates in AddProductionRecord form

--- src/main/java/org/chainoptim/desktop/features/factory/controller/factoryproduction/AddProductionRecordController.java
+++ src/main/java/org/chainoptim/desktop/features/factory/controller/factoryproduction/AddProductionRecordController.java
@@ -38,6 +38,7 @@ public class AddProductionRecordController implements DataReceiver<Factory> {
     private Factory factory;
     private DailyProductionRecord newRecord;
     private Map<Integer, DailyProductionRecord> recordsByStageId;
+    private float previousDuration = 1;
 
     @FXML
     private Label currentPlanStartDate;
@@ -74,53 +75,30 @@ public class AddProductionRecordController implements DataReceiver<Factory> {
     }
 
     private void setUpListeners() {
-        startDatePicker.valueProperty().addListener((observable, oldValue, newValue) -> rerenderPlan());
+        startDatePicker.valueProperty().addListener((observable, oldValue, newValue) -> adjustPlanForDuration());
 
-        endDatePicker.valueProperty().addListener((observable, oldValue, newValue) -> rerenderPlan());
+        endDatePicker.valueProperty().addListener((observable, oldValue, newValue) -> adjustPlanForDuration());
     }
 
-    private void rerenderPlan() {
+    private void adjustPlanForDuration() {
         if (currentPlan == null) return;
         if (startDatePicker.getValue() == null || endDatePicker.getValue() == null) return;
 
         // Calculate record duration
         long recordDuration = endDatePicker.getValue().toEpochDay() - startDatePicker.getValue().toEpochDay();
-        if (recordDuration < 0) return;
+        if (recordDuration <= 0) return;
 
-        // Create a new plan based on the original plan
-        AllocationPlan adjustedPlan = createAdjustedPlan(recordDuration);
+        // Adjust the plan for the new duration
+        float newDuration = recordDuration / previousDuration;
+        if (previousDuration != 1) {
+            newDuration *= currentPlan.getAllocationPlan().getDurationDays();
+        }
+        currentPlan.getAllocationPlan().adjustForDuration(newDuration);
+        previousDuration = recordDuration;
 
         // Display the form with the adjusted plan
-        displayFormWithPlan(adjustedPlan);
+        displayFormWithPlan(currentPlan.getAllocationPlan());
     }
-
-    private AllocationPlan createAdjustedPlan(long recordDuration) {
-        if (currentPlan == null || currentPlan.getAllocationPlan() == null) {
-            return null; // Handle the case where the current plan or its allocation plan is null
-        }
-
-        AllocationPlan originalPlan = currentPlan.getAllocationPlan(); // Get the original plan
-
-        AllocationPlan adjustedPlan = new AllocationPlan();
-        // Copy relevant fields from the original plan
-        adjustedPlan.setAllocations(new ArrayList<>(originalPlan.getAllocations()));
-        adjustedPlan.setResults(new ArrayList<>(originalPlan.getResults()));
-        adjustedPlan.setDurationDays(originalPlan.getDurationDays());
-
-        // Adjust allocations and results for the new duration
-        float durationRatio = recordDuration / originalPlan.getDurationDays();
-        for (ResourceAllocation allocation : adjustedPlan.getAllocations()) {
-            allocation.setRequestedAmount(allocation.getRequestedAmount() * durationRatio);
-            allocation.setAllocatedAmount(allocation.getAllocatedAmount() * durationRatio);
-        }
-        for (AllocationResult result : adjustedPlan.getResults()) {
-            result.setFullAmount(result.getFullAmount() * durationRatio);
-            result.setResultedAmount(result.getResultedAmount() * durationRatio);
-        }
-
-        return adjustedPlan;
-    }
-
 
     private void loadCurrentPlan(Integer factoryId) {
         fallbackManager.reset();
@@ -138,7 +116,6 @@ public class AddProductionRecordController implements DataReceiver<Factory> {
                 return;
             }
             currentPlan = planOptional.get();
-            System.out.println("Current Plan: " + currentPlan);
             fallbackManager.setLoading(false);
 
             currentPlanStartDate.setText(currentPlan.getActivationDate().toLocalDate().toString());
@@ -164,66 +141,11 @@ public class AddProductionRecordController implements DataReceiver<Factory> {
         groupRecordsByStageId(allocationPlan);
 
         for (Map.Entry<Integer, DailyProductionRecord> entry : recordsByStageId.entrySet()) {
-            Label stageNameLabel = addStageLabel(entry.getValue().getAllocations().getFirst().getStageName());
-            allocationsVBox.getChildren().add(stageNameLabel);
+            renderStageLabel(entry.getValue().getAllocations().getFirst().getStageName());
 
-            // Stage Inputs
-            Label stageInputLabel = new Label("• Stage Inputs");
-            stageInputLabel.setStyle("-fx-padding: 8px 0 0 0; -fx-font-size: 16px; -fx-font-weight: bold;");
-            allocationsVBox.getChildren().add(stageInputLabel);
+            renderStageInputs(entry.getValue().getAllocations());
 
-            if (entry.getValue().getAllocations() == null) {
-                stageInputLabel.setText("Stage Inputs: None");
-                continue;
-            }
-
-            GridPane stageInputGridPane = new GridPane();
-            stageInputGridPane.setHgap(16);
-            stageInputGridPane.setVgap(8);
-            stageInputGridPane.setPadding(new Insets(8));
-
-            // - Headers
-            renderColumnHeader(stageInputGridPane, "Component", 0);
-            renderColumnHeader(stageInputGridPane, "Actual Amount", 1);
-            renderColumnHeader(stageInputGridPane, "Planned Amount", 2);
-            renderColumnHeader(stageInputGridPane, "Needed Amount", 3);
-
-            // - Rows
-            for (int i = 0; i < entry.getValue().getAllocations().size(); i++) {
-                renderStageInputGridPane(stageInputGridPane, entry.getValue().getAllocations().get(i), i + 1);
-            }
-
-            stageInputsGridPanes.add(stageInputGridPane);
-            allocationsVBox.getChildren().add(stageInputGridPane);
-
-            // Stage Outputs
-            Label stageOutputLabel = new Label("• Stage Outputs");
-            stageOutputLabel.setStyle("-fx-padding: 8px 0 0 0; -fx-font-size: 16px; -fx-font-weight: bold;");
-            allocationsVBox.getChildren().add(stageOutputLabel);
-
-            if (entry.getValue().getResults() == null) {
-                stageOutputLabel.setText("Stage Outputs: None");
-                continue;
-            }
-
-            GridPane stageOutputGridPane = new GridPane();
-            stageOutputGridPane.setHgap(16);
-            stageOutputGridPane.setVgap(8);
-            stageOutputGridPane.setPadding(new Insets(8));
-
-            // - Headers
-            renderColumnHeader(stageOutputGridPane, "Component", 0);
-            renderColumnHeader(stageOutputGridPane, "Actual Amount", 1);
-            renderColumnHeader(stageOutputGridPane, "Expected Amount", 2);
-            renderColumnHeader(stageOutputGridPane, "Full Amount", 3);
-
-            // - Rows
-            for (int i = 0; i < entry.getValue().getResults().size(); i++) {
-                renderStageOutputGridPane(stageOutputGridPane, entry.getValue().getResults().get(i), i + 1);
-            }
-
-            stageOutputsGridPanes.add(stageOutputGridPane);
-            allocationsVBox.getChildren().add(stageOutputGridPane);
+            renderStageOutputs(entry.getValue().getResults());
         }
     }
 
@@ -252,11 +174,71 @@ public class AddProductionRecordController implements DataReceiver<Factory> {
         }
     }
 
-    private Label addStageLabel(String stageName) {
+    private void renderStageLabel(String stageName) {
         Label stageNameLabel = new Label("Stage: " + stageName);
         stageNameLabel.getStyleClass().setAll("general-label-large");
         stageNameLabel.setStyle("-fx-padding: 8px 0 0 0;");
-        return stageNameLabel;
+        allocationsVBox.getChildren().add(stageNameLabel);
+    }
+
+    private void renderStageInputs(List<ResourceAllocation> allocations) {
+        Label stageInputLabel = new Label("• Stage Inputs");
+        stageInputLabel.setStyle("-fx-padding: 8px 0 0 0; -fx-font-size: 16px; -fx-font-weight: bold;");
+        allocationsVBox.getChildren().add(stageInputLabel);
+
+        if (allocations == null) {
+            stageInputLabel.setText("Stage Inputs: None");
+            return;
+        }
+
+        GridPane stageInputGridPane = new GridPane();
+        stageInputGridPane.setHgap(16);
+        stageInputGridPane.setVgap(8);
+        stageInputGridPane.setPadding(new Insets(8));
+
+        // - Headers
+        renderColumnHeader(stageInputGridPane, "Component", 0);
+        renderColumnHeader(stageInputGridPane, "Actual Amount", 1);
+        renderColumnHeader(stageInputGridPane, "Planned Amount", 2);
+        renderColumnHeader(stageInputGridPane, "Needed Amount", 3);
+
+        // - Rows
+        for (int i = 0; i < allocations.size(); i++) {
+            renderStageInputGridPane(stageInputGridPane, allocations.get(i), i + 1);
+        }
+
+        stageInputsGridPanes.add(stageInputGridPane);
+        allocationsVBox.getChildren().add(stageInputGridPane);
+    }
+
+    private void renderStageOutputs(List<AllocationResult> results) {
+        Label stageOutputLabel = new Label("• Stage Outputs");
+        stageOutputLabel.setStyle("-fx-padding: 8px 0 0 0; -fx-font-size: 16px; -fx-font-weight: bold;");
+        allocationsVBox.getChildren().add(stageOutputLabel);
+
+        if (results == null) {
+            stageOutputLabel.setText("Stage Outputs: None");
+            return;
+        }
+
+        GridPane stageOutputGridPane = new GridPane();
+        stageOutputGridPane.setHgap(16);
+        stageOutputGridPane.setVgap(8);
+        stageOutputGridPane.setPadding(new Insets(8));
+
+        // - Headers
+        renderColumnHeader(stageOutputGridPane, "Component", 0);
+        renderColumnHeader(stageOutputGridPane, "Actual Amount", 1);
+        renderColumnHeader(stageOutputGridPane, "Expected Amount", 2);
+        renderColumnHeader(stageOutputGridPane, "Full Amount", 3);
+
+        // - Rows
+        for (int i = 0; i < results.size(); i++) {
+            renderStageOutputGridPane(stageOutputGridPane, results.get(i), i + 1);
+        }
+
+        stageOutputsGridPanes.add(stageOutputGridPane);
+        allocationsVBox.getChildren().add(stageOutputGridPane);
     }
 
     private void renderColumnHeader(GridPane stageGridPane, String headerText, int columnIndex) {
@@ -271,43 +253,30 @@ public class AddProductionRecordController implements DataReceiver<Factory> {
     }
 
     private void renderStageInputGridPane(GridPane stageGridPane, ResourceAllocation allocation, int rowIndex) {
-        Label componentNameLabel = new Label(allocation.getComponentName());
-        componentNameLabel.getStyleClass().setAll("general-label");
-        stageGridPane.add(componentNameLabel, 0, rowIndex);
+        renderLabel(stageGridPane, allocation.getComponentName(), rowIndex, 0);
 
         TextField actualAmountField = new TextField();
         actualAmountField.getStyleClass().setAll("custom-text-field");
         stageGridPane.add(actualAmountField, 1, rowIndex);
 
-        Label plannedAmountValue = new Label(String.valueOf(allocation.getAllocatedAmount()));
-        plannedAmountValue.getStyleClass().setAll("general-label");
-        plannedAmountValue.setStyle("-fx-text-fill: #121212");
-        stageGridPane.add(plannedAmountValue, 2, rowIndex);
-
-        Label neededAmountValue = new Label(String.valueOf(allocation.getRequestedAmount()));
-        neededAmountValue.getStyleClass().setAll("general-label");
-        neededAmountValue.setStyle("-fx-text-fill: #121212");
-        stageGridPane.add(neededAmountValue, 3, rowIndex);
+        renderLabel(stageGridPane, String.valueOf(allocation.getAllocatedAmount()), rowIndex, 2);
+        renderLabel(stageGridPane, String.valueOf(allocation.getRequestedAmount()), rowIndex, 3);
     }
 
     private void renderStageOutputGridPane(GridPane stageGridPane, AllocationResult allocationResult, int rowIndex) {
-        Label componentNameLabel = new Label(allocationResult.getComponentName());
-        componentNameLabel.getStyleClass().setAll("general-label");
-        componentNameLabel.setStyle("-fx-padding: 4px;");
-        stageGridPane.add(componentNameLabel, 0, rowIndex);
+        renderLabel(stageGridPane, allocationResult.getComponentName(), rowIndex, 0);
 
         TextField actualAmountField = new TextField();
         actualAmountField.getStyleClass().setAll("custom-text-field");
         stageGridPane.add(actualAmountField, 1, rowIndex);
 
-        Label expectedAmountValue = new Label(String.valueOf(allocationResult.getResultedAmount()));
-        expectedAmountValue.getStyleClass().setAll("general-label");
-        expectedAmountValue.setStyle("-fx-text-fill: #121212");
-        stageGridPane.add(expectedAmountValue, 2, rowIndex);
+        renderLabel(stageGridPane, String.valueOf(allocationResult.getResultedAmount()), rowIndex, 2);
+        renderLabel(stageGridPane, String.valueOf(allocationResult.getFullAmount()), rowIndex, 3);
+    }
 
-        Label fullAmountValue = new Label(String.valueOf(allocationResult.getFullAmount()));
-        fullAmountValue.getStyleClass().setAll("general-label");
-        fullAmountValue.setStyle("-fx-text-fill: #121212");
-        stageGridPane.add(fullAmountValue, 3, rowIndex);
+    private void renderLabel(GridPane stageGridPane, String text, int rowIndex, int columnIndex) {
+        Label label = new Label(text);
+        label.getStyleClass().setAll("general-label");
+        stageGridPane.add(label, columnIndex, rowIndex);
     }
 }
